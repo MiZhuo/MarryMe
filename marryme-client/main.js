a56def822ca9e5b90af18db6ccc3a491ed154ce4
mizhuo <wulibin0809@gmail.com>
2022-10-31 22:48:04 +0800

    添加axios

--- marryme-client/main.js
+++ marryme-client/main.js
@@ -20,3 +20,33 @@ export function createApp() {
   }
 }
 // #endif
+
+import axios from 'axios'
+ 
+// 解决uniapp 适配axios请求，避免报adapter is not a function错误
+axios.defaults.adapter = config => {
+	return new Promise((resolve, reject) => {
+		let settle = require('axios/lib/core/settle');
+		let buildURL = require('axios/lib/helpers/buildURL');
+		uni.request({
+			method: config.method.toUpperCase(),
+			url: config.baseURL + buildURL(config.url, config.params, config.paramsSerializer),
+			header: config.headers,
+			data: config.data,
+			dataType: config.dataType,
+			responseType: config.responseType,
+			sslVerify: config.sslVerify,
+			complete: function complete(response) {
+				// console.log("执行完成：", response)
+				response = {
+					data: response.data,
+					status: response.statusCode,
+					errMsg: response.errMsg,
+					header: response.header,
+					config: config
+				};
+				settle(resolve, reject, response);
+			}
+		})
+	})
+}
