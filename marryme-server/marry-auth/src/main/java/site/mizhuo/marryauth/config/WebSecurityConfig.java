0f1ca27671b7d129c4389c9e1dfa2ed5c38d40f9
mizhuo <wulibin0809@gmail.com>
2022-11-01 22:41:09 +0800

    添加common代码

--- marryme-server/marry-auth/src/main/java/site/mizhuo/marryauth/config/WebSecurityConfig.java
+++ marryme-server/marry-auth/src/main/java/site/mizhuo/marryauth/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package site.mizhuo.marryauth.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,19 +11,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * @ClassName SecurityConfig
- * @Description:
- * @Author: MiZhuo
- * @Create: 2022-11-01 15:04
- * @Version 1.0
- **/
+ * SpringSecurity配置
+ * Created by macro on 2020/6/19.
+ */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                .antMatchers("/rsa/publicKey").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/oauth/token").permitAll()
+                .anyRequest().authenticated();
     }
 
     @Bean
@@ -31,17 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/oauth/**", "/login/**", "/logout/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .permitAll();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
 }
---
+++ marryme-server/marry-common/src/main/java/site/mizhuo/marrycommon/domain/UserDto.java
@@ -0,0 +1,26 @@
+package site.mizhuo.marrycommon.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 登录用户信息
+ *
+ * @author macro
+ * @date 2020/6/19
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String password;
+    private Integer status;
+    private String clientId;
+    private List<String> roles;
+
+}
