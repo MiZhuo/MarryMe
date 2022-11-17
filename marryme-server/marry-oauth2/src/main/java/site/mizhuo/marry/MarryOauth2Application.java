ad6e4395b1a7fdb16a05befb5f582b3ecfa8a595
mizhuo <wulibin0809@gmail.com>
2022-11-17 22:50:05 +0800

    添加oauth2测试代码

--- marryme-server/marry-oauth2/src/main/java/site/mizhuo/marry/MarryOauth2Application.java
+++ marryme-server/marry-oauth2/src/main/java/site/mizhuo/marry/MarryOauth2Application.java
@@ -2,8 +2,10 @@ package site.mizhuo.marry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MarryOauth2Application {
 
     public static void main(String[] args) {
