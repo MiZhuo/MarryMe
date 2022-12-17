73c894794c397ed46ce8d9fffce8e5727725bce5
mizhuo <wulibin0809@gmail.com>
2022-12-17 22:07:13 +0800

    新增mybatis-plus-extension依赖,修复分页问题

--- marryme-server/marry-friends/src/main/java/site/mizhuo/marry/friends/config/MyBatisConfig.java
+++ marryme-server/marry-friends/src/main/java/site/mizhuo/marry/friends/config/MyBatisConfig.java
@@ -1,6 +1,10 @@
 package site.mizhuo.marry.friends.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,4 +16,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan("site.mizhuo.marry.friends.mapper")
 public class MyBatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
 }
